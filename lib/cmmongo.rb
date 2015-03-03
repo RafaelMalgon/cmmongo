85ad4fead380192e30392a1bdae36c455a530f40
Edwin Páez <edwin@grability.com>
2015-03-02 22:49:55 -0500

    booting capabilities

--- lib/cmmongo.rb
+++ lib/cmmongo.rb
@@ -1,5 +1,19 @@
 require "cmmongo/version"
 
 module CMMongo
-  # Your code goes here...
+  @@blocks = []
+  def self.booting &block
+    (@@blocks ||= []) << block
+  end
+
+  def self.boot env = :production
+    @@env = env
+    @@blocks.each do |block|
+      block.call
+    end
+  end
+
+  def self.env
+    @@env
+  end
 end
--- spec/cmmongo_spec.rb
+++ spec/cmmongo_spec.rb
@@ -3,5 +3,21 @@ require 'spec_helper'
 require 'cmmongo'
 
 describe "CMMongo" do
-  
+  it "calls blocks on booting" do
+    they_called = false
+    CMMongo.booting do
+      they_called = true
+    end
+    CMMongo.boot
+    expect(they_called).to eql(true)
+  end
+
+  it "has environment" do
+    CMMongo.boot :development
+    expect(CMMongo.env).to eql(:development)
+  end
+  it "defaults to production environment" do
+    CMMongo.boot
+    expect(CMMongo.env).to eql(:production)
+  end
 end
